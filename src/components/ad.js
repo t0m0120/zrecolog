2b306ab174f1c9b59c56f8927539917d257429e4
t0m0120 <highwest.t@gmail.com>
2019-04-28 22:15:13 +0900

    add ad

---
+++ src/components/ad.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AdSense from 'react-adsense'
+
+const Ad = () => (
+  <div>
+      <AdSense.Google
+        client="ca-pub-5441589109590198"
+        slot="7229475060"
+        format="auto"
+        />
+    </div>
+  )
+export default Ad;
