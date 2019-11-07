82b7663fa8838bd17420caa07e622f2c02b8336c
t0m0120 <highwest.t@gmail.com>
2019-11-07 22:43:54 +0900

    update

--- src/pages/comikiri_update.js
+++ src/pages/comikiri_update.js
@@ -3,18 +3,19 @@ import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
-class PrivacyPolicy extends React.Component {
+class UpdateInfo extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="PrivacyPolicy"
+          title="UpdateInfo"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-                    <h2>2019.11.07</h2>
-                    <p>AppStore初回審査</p>
+          <h2>アップデート情報</h2>
+          <h3>2019/11/07</h3>
+          <p>AppStore初回審査</p>
       </Layout>
     )
   }
